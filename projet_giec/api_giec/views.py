9b855e07c443243e2517993fe8fbb2a216417d81
metenier <meteniermyriam@yahoo.fr>
2025-05-26 00:38:14 +0200

    mau

--- projet_giec/api_giec/views.py
+++ projet_giec/api_giec/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
 from rest_framework import viewsets
+
 from .models import RapportClimatique
 from .serializers import RapportClimatiqueSerializer
+
 
 class RapportClimatiqueViewSet(viewsets.ModelViewSet):
     queryset = RapportClimatique.objects.all()
