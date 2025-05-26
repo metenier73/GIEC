c9dff59003468732a1ad1ffca38f517d8dccc72a
metenier <meteniermyriam@yahoo.fr>
2025-05-26 20:36:04 +0200

    AA

--- projet_giec/api_giec/views.py
+++ projet_giec/api_giec/views.py
@@ -1,8 +1,15 @@
-# Create your views here.
-from rest_framework import viewsets
 
+from django.http import HttpResponse
+from rest_framework import viewsets
 from .models import RapportClimatique
 from .serializers import RapportClimatiqueSerializer
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Bienvenue sur l'API GIEC ! Accédez à /api/ pour voir les données.")
+
+
 
 
 class RapportClimatiqueViewSet(viewsets.ModelViewSet):
